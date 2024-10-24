db31bf07e0d1b3ffb325b63d05dd69742c9d440b
Felix Xi <f-xi@onu.edu>
2024-10-24 13:11:30 -0400

    completed countWords function.

--- Lab8p2/Source.cpp
+++ Lab8p2/Source.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int countWords(string str);
+int main() {
+	int numwords;
+	bool Qcheck = true;
+	string input;
+	while (Qcheck) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q") break ;
+		numwords = countWords(input);
+		cout << "Word count: " << numwords << endl;
+	}
+	return 0;
+}
+
+
+
+int countWords(string str) {
+	int wordcount = 1;
+	for (int i = 0; i < str.length(); i++) {
+		char c = str[i];
+		char b = str[i + 1];
+		 if (c == ' ' && b!=' ') {
+			wordcount++;
+		}
+	}
+	return wordcount;
+}
